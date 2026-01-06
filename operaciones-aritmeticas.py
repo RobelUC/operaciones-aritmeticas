7173b4aac1f95cfd02d78c792fe81f3f5336491a
RobelUC <73531878@continental.edu.pe>
2026-01-06 18:44:40 -0500

    Simplifación de código

--- operaciones-aritmeticas.py
+++ operaciones-aritmeticas.py
@@ -1,4 +1,6 @@
+def suma_dos_numeros(numero1, numero2):
+    return numero1 + numero2
+
 numero1 = int(input("Ingrese el primer número: "))
 numero2 = int(input("Ingrese el segundo número: "))
-suma = numero1 + numero2
-print("La suma de", numero1, "y", numero2, "es:", suma)
+print("La suma de", numero1, "y", numero2, "es:", suma_dos_numeros(numero1, numero2))
